aded393947b3bb63887b07da8913453e8cc68061
WouterJanssens98 <woutjans1@student.arteveldehs.be>
2020-08-06 21:15:22 +0200

    cleaned repo

--- src/templates/helpers/bold.js
+++
@@ -1,6 +0,0 @@
-import Handlebars from 'handlebars';
-
-export default (text) => {
-  const escapedText = Handlebars.escapeExpression(text);
-  return text !== '' ? new Handlebars.SafeString(`<strong>${escapedText}</strong>`) : '';
-};
